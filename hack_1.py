e719aba82af6642575629f76072de80eb0806e3f
Angxandralol <angyeemarin.dev@gmail.com>
2024-05-19 21:12:16 -0400

    feat: hack 1 completed

--- hack_1.py
+++ hack_1.py
@@ -6,7 +6,16 @@ text: "qux" output => "qUx"
 text: "eq" output => "eq" 
 """
 
-
 def fn_hack_1(s):
     result = s
+    txt = ''
+    i = 1
+    for letter in result:
+        if i == 3: 
+            middle_letter = txt[-1].upper()
+            txt = txt[:-1] + middle_letter
+            i = 1
+        else: i += 1
+        txt += letter
+    result = txt
     return result
