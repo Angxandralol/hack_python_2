41b0355020506eb7e8220915f2b7c22480ca319d
Angxandralol <angyeemarin.dev@gmail.com>
2024-05-21 09:04:13 -0400

    feat: hack 3 completed

--- hack_3.py
+++ hack_3.py
@@ -17,5 +17,13 @@ text: "qux" output => "QvX"
 
 def fn_hack_3(s):
     result = s
-    #...
+    result = result.replace('a', '@')
+    result = result.replace('e', '3')
+    result = result.replace('i', '¡')
+    result = result.replace('o', '0')
+    result = result.replace('u', 'v')
+    if result[-1] != 'v': 
+        result = result[::-1].capitalize()
+        result = result[::-1]
+    result = result[0].upper() + result[1:]
     return result
