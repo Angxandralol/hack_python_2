40e7be75cc0144fbd8f1a1626e721370255cfa38
Angxandralol <angyeemarin.dev@gmail.com>
2024-05-21 09:50:10 -0400

    feat: hack 6 completed

--- hack_6.py
+++ hack_6.py
@@ -8,8 +8,12 @@ text: ["a","b","c","d","e"] output => ["1","-","3","-","5"]
 text: [] output => ["0"] 
 """
 
-
 def fn_hack_6(s):
     result = s
-    #...
-    return result
+    txt = []
+    if len(result) > 0: 
+        for i in range(1, len(result) + 1):
+            if i % 2 == 0: txt.append("-")
+            else: txt.append(str(i))
+    else: txt.append("0")
+    return txt
