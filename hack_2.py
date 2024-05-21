7995621b3eac921a46d238b6ccadc4dcc962dd0c
Angxandralol <angyeemarin.dev@gmail.com>
2024-05-21 08:30:57 -0400

    feat: hack 2 completed

--- hack_2.py
+++ hack_2.py
@@ -9,5 +9,8 @@ text: "qux" output => "qx"
 
 def fn_hack_2(s):
     result = s
-    #...
-    return result
+    vocals = ['a', 'e', 'i', 'o', 'u']
+    txt = ''
+    for letter in result:
+        if letter not in vocals: txt += letter
+    return txt
