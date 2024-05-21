f93bcf0f6e534d6d887bfcc90a19832a9e6e413c
Angxandralol <angyeemarin.dev@gmail.com>
2024-05-21 10:25:09 -0400

    feat: hack 9 completed

--- hack_9.py
+++ hack_9.py
@@ -5,5 +5,8 @@ text: {"foo":"fookziman","bar":"barziman"} output => {"Foo":"Fooziman"}
 
 def fn_hack_9(s):
     result = s
-    #...
+    if "foo" in result:
+        result["foo"] = "Fooziman"
+        result["Foo"] = result.pop("foo")
+        result.pop("bar")
     return result
