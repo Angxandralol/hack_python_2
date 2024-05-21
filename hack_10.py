f35c47ed80d120a49786f7aca73810b06ca82f15
Angxandralol <angyeemarin.dev@gmail.com>
2024-05-21 10:34:27 -0400

    feat: hack 10 completed

--- hack_10.py
+++ hack_10.py
@@ -5,5 +5,9 @@ text: [{"a":"b"},{"c","d"},{"e":"f"}] output => [{"1":"2"},{"3","4"},{"5":"6"}]
 
 def fn_hack_10(s):
     result = s
-    #...
-    return result
+    new_list = []
+    counter = 1
+    for i in range(len(result)):
+        new_list.append({str(counter): str(counter + 1)})
+        counter += 2
+    return new_list
